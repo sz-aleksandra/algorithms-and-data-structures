7e65f698a5f98b1922c460afb5e3955f1603388f
Mikolaj Rozek <mikolajrozek0@gmail.com>
2023-03-16 15:26:49 +0100

    corrected not pycache it was __init__.py, and implemented the correct list of words from file

--- Sorts/MergeSort/merge_sort.py
+++ Sorts/MergeSort/merge_sort.py
@@ -1,7 +1,6 @@
+from ..file_reading import read_from_file
 
 
+def merge(list_of_words):
 
 
-
-
-def merge()
--- Sorts/file_reading.py
+++ Sorts/file_reading.py
@@ -2,6 +2,15 @@
 def read_from_file(filepath):
     with open(filepath, 'r') as file_handle:
         lines = [line.rstrip() for line in file_handle]
-    return lines
+    return [line for line in lines if line]
 
 
+def line_division_to_wordlist(linelist):
+    wordlist = []
+    for line in linelist:
+        line_wordlist = line.split()
+        wordlist += line_wordlist
+    return wordlist
+
+
+print(line_division_to_wordlist(read_from_file('pan-tadeusz-unix.txt')))
