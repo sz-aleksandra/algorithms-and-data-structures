ca6bcf8eb365f05983caaa7f808c096aff8284e7
Mikolaj Rozek <mikolajrozek0@gmail.com>
2023-03-16 16:03:27 +0100

    end for today

--- Sorts/MergeSort/merge_sort.py
+++ Sorts/MergeSort/merge_sort.py
@@ -1,14 +1,19 @@
 
 def merge(list_of_words):
     if len(list_of_words) > 1:
+
         half = len(list_of_words) // 2
         left_half = list_of_words[:half]
         right_half = list_of_words[half:]
-        i, j = 0
-        for word in list_of_words:
-            if left_half[i] <= right_half[j]:
-                word = left_half[i]
-                i += 1
+
+        left_index = right_index = list_index = 0
+
+        while left_index < len(left_half) and right_index < len(right_half):
+            if left_half[left_index] <= right_half[right_index]:
+                list_of_words[list_index] = left_half[left_index]
+                left_index += 1
             else:
-                word == right_half[j]
-                j += 1
+                list_of_words[list_index] = right_half[right_index]
+                right_index += 1
+            list_index += 1
+
