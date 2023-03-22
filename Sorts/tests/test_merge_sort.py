9eeea3d72eadb527fce374be95a07aa339abf5fd
Mikolaj Rozek <mikolajrozek0@gmail.com>
2023-03-22 12:50:09 +0100

    dasdasda

--- Sorts/tests/test_merge_sort.py
+++
@@ -1,6 +0,0 @@
-from ..MergeSort.merge_sort import merge_sort
-
-
-def test_merge_sort(sample_list):
-    expected_list = sorted(sample_list)
-    assert merge_sort(sample_list) == expected_list
--- Sorts/tests/test_sorts.py
+++ Sorts/tests/test_sorts.py
@@ -1,5 +1,10 @@
+from ..MergeSort.merge_sort import merge_sort
 from ..BubbleSort.bubble_sort import bubble_sort
 
+
+def test_merge_sort(sample_list):
+    expected_list = sorted(sample_list)
+    assert merge_sort(sample_list) == expected_list
 
 def test_bubble_sort(sample_list):
     expected_list = sorted(sample_list)
