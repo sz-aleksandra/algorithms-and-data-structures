fb9653efb726b3a951bf03d13f9b31c1e684900d
Mikolaj Rozek <mikolajrozek0@gmail.com>
2023-03-22 11:11:10 +0100

    completed merge function

--- Sorts/MergeSort/merge_sort.py
+++ Sorts/MergeSort/merge_sort.py
@@ -12,8 +12,20 @@ def merge(list_of_words):
             if left_half[left_index] <= right_half[right_index]:
                 list_of_words[list_index] = left_half[left_index]
                 left_index += 1
+
             else:
                 list_of_words[list_index] = right_half[right_index]
                 right_index += 1
             list_index += 1
+
+        while left_index < len(left_half):
+            list_of_words[list_index] = left_half[left_index]
+            list_index += 1
+            left_index += 1
+
+        while right_index < len(right_half):
+            list_of_words[list_index] = right_half[right_index]
+            list_index += 1
+            right_index += 1
+
 
