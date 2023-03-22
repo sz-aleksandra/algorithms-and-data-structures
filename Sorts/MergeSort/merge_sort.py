70bde38890f3c4768eea6f8145eae3d10b10b387
Mikolaj Rozek <mikolajrozek0@gmail.com>
2023-03-22 11:28:18 +0100

    corrected the algorithm for merge sort

--- Sorts/MergeSort/merge_sort.py
+++ Sorts/MergeSort/merge_sort.py
@@ -1,31 +1,37 @@
 
-def merge(list_of_words):
-    if len(list_of_words) > 1:
+def merge(left_half, right_half):
+    sorted_list = []
+    left_index = 0
+    right_index = 0
 
-        half = len(list_of_words) // 2
-        left_half = list_of_words[:half]
-        right_half = list_of_words[half:]
-
-        left_index = right_index = list_index = 0
-
-        while left_index < len(left_half) and right_index < len(right_half):
-            if left_half[left_index] <= right_half[right_index]:
-                list_of_words[list_index] = left_half[left_index]
-                left_index += 1
-
-            else:
-                list_of_words[list_index] = right_half[right_index]
-                right_index += 1
-            list_index += 1
-
-        while left_index < len(left_half):
-            list_of_words[list_index] = left_half[left_index]
-            list_index += 1
+    while left_index < len(left_half) and right_index < len(right_half):
+        if left_half[left_index] <= right_half[right_index]:
+            sorted_list.append(left_half[left_index])
             left_index += 1
-
-        while right_index < len(right_half):
-            list_of_words[list_index] = right_half[right_index]
-            list_index += 1
+        else:
+            sorted_list.append(right_half[right_index])
             right_index += 1
 
+    while left_index < len(left_half):
+        sorted_list.append(left_half[left_index])
+        left_index += 1
 
+    while right_index < len(right_half):
+        sorted_list.append(right_half[right_index])
+        right_index += 1
+
+    return sorted_list
+
+
+def merge_sort(list_for_sort):
+    if len(list_for_sort) <= 1:
+        return list_for_sort
+
+    mid_index = len(list_for_sort) // 2
+    list_left_half = list_for_sort[:mid_index]
+    list_right_half = list_for_sort[mid_index:]
+
+    list_left_half = merge_sort(list_left_half)
+    list_right_half = merge_sort(list_right_half)
+
+    return merge(list_left_half, list_right_half)
