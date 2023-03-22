aac8577964a75418f1c48b05b5eb8f196788ed10
Mikolaj Rozek <mikolajrozek0@gmail.com>
2023-03-22 12:55:12 +0100

    removed the conftest.py file, as it is more clear with fixture in testfile

--- Sorts/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-@pytest.fixture
-def sample_list():
-    return ['a', 'b', 'c', 'd', 'E', 'A', 'Abecadło',
-            '123', '1', '0', 'Litwo', 'Pan', 'Tadeusz', 'Mickiewicz']
--- Sorts/tests/test_sorts.py
+++ Sorts/tests/test_sorts.py
@@ -1,5 +1,13 @@
 from ..MergeSort.merge_sort import merge_sort
 from ..BubbleSort.bubble_sort import bubble_sort
+from ..SelectionSort.selection_sort import selection_sort
+import pytest
+
+
+@pytest.fixture
+def sample_list():
+    return ['a', 'b', 'c', 'd', 'E', 'A', 'Abecadło',
+            '123', '1', '0', 'Litwo', 'Pan', 'Tadeusz', 'Mickiewicz']
 
 
 def test_merge_sort(sample_list):
@@ -10,4 +18,10 @@ def test_merge_sort(sample_list):
 def test_bubble_sort(sample_list):
     expected_list = sorted(sample_list)
     bubble_sort(sample_list)
+    assert sample_list == expected_list
+
+
+def test_selection_sort(sample_list):
+    expected_list = sorted(sample_list)
+    selection_sort(sample_list)
     assert sample_list == expected_list
