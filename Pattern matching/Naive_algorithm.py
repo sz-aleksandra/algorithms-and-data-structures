8ab3f14b3c6543849ae5e7bfdd100c6c2443a29e
Aleksandra Szymańska <01169248@pw.edu.pl>
2023-05-03 02:47:32 +0200

    seems legit

--- Pattern matching/Naive_algorithm.py
+++ Pattern matching/Naive_algorithm.py
@@ -6,21 +6,31 @@ def get_text(path):
     return text
 
 
+def get_list_of_words(text):
+    alphabet = 'aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźżAĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSTUVWXYZŹŻ'
+    words = []
+    word = ''
+    for character in text:
+        if character in alphabet:
+            word += character
+        elif word:
+            words.append(word)
+            word = ''
+    words = list(set(words))
+    return words
+
+
 #print(get_text('../Sorts/pan-tadeusz-unix.txt'))
 
 
 def find_N(string, text):
     position_list = []
     pos_in_text = 0
-    while pos_in_text < len(text) - len(string):
+    while pos_in_text <= len(text) - len(string):
         pos_in_string = 0
-        does_it_match_possibly = True
-        while does_it_match_possibly and pos_in_string < len(string):
-            if text[pos_in_text + pos_in_string] != string[pos_in_string]:
-                does_it_match_possibly = False
-            else:
-                pos_in_string += 1
-        if does_it_match_possibly:
+        while pos_in_string < len(string) and text[pos_in_text + pos_in_string] == string[pos_in_string]:
+            pos_in_string += 1
+        if pos_in_string == len(string):
             position_list.append(pos_in_text)
             pos_in_text += len(string)
         else:
@@ -28,7 +38,33 @@ def find_N(string, text):
     return position_list
 
 
-#example_text = 'lokomotywamotywa'
-#example_string = 'motyw'
+def create_KMP_table(string):
+    KMP_table = [0, 0]
+    k = 0
+    for pos_in_kmp in range(1, len(string)):
+        while k > 0 and string[k] != string[pos_in_kmp]:
+            k = KMP_table[k]
+        if string[k] == string[pos_in_kmp]:
+            k += 1
+        KMP_table.append(k)
+    return KMP_table
 
+
+def find_KMP(string, text):
+    KMP_table = create_KMP_table(string)
+    position_list = []
+    pos_in_text = 0
+    while pos_in_text <= len(text) - len(string):
+        pos_in_string = KMP_table[0]
+        while pos_in_string < len(string) and text[pos_in_text + pos_in_string] == string[pos_in_string]:
+            pos_in_string += 1
+        if pos_in_string == len(string):
+            position_list.append(pos_in_text)
+        pos_in_text += max(1, pos_in_string - KMP_table[pos_in_string])
+    return position_list
+
+#example_text = 'lokomotywmotywa'
+#xample_string = 'motyw'
 #print(find_N(example_string, example_text))
+#print(find_KMP(example_string, example_text))
+
