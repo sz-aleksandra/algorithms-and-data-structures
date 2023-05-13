3a9d511fd2c341533a1c814e45abf4e88dd2c7be
Aleksandra Szymańska <01169248@pw.edu.pl>
2023-05-13 18:40:10 +0200

    after checking

--- Pattern matching/plot_maker.py
+++ Pattern matching/plot_maker.py
@@ -1,76 +1,75 @@
-from Naive_algorithm import get_text, get_list_of_words, find_N, find_KMP, find_KR
-from time import process_time
 import matplotlib.pyplot as plt
+from time import process_time
 import gc
+from Naive_algorithm import find_N, find_KMP, get_text, get_list_of_words
+from kr_algorithm import find_KR
 
 
-def get_n_first_words(n, words):
-    no_duplicates = list(set(words))
-    return no_duplicates[:n]
-
-
-def measure_find_N_timew(n_values, text):
+def measure_find_N_times(n_values, list_of_words, text):
     finding_times = []
     gc_old = gc.isenabled()
     gc.disable()
     for n in n_values:
-        first_n_words = get_n_first_words(n, get_list_of_words(text))
         start_time = process_time()
-        for word in first_n_words:
-            find_N(word, text)
+        for i in range(n):
+            find_N(list_of_words[i], text)
         end_time = process_time()
-        finding_times.append(end_time - start_time)
-        print(end_time - start_time)
+        result = end_time - start_time
+        finding_times.append(result)
+        print(f"N: {result}")
     if gc_old:
         gc.enable()
     return finding_times
 
 
-def measure_find_KMP_timew(n_values, text):
+def measure_find_KMP_times(n_values, list_of_words, text):
     finding_times = []
     gc_old = gc.isenabled()
     gc.disable()
     for n in n_values:
-        first_n_words = get_n_first_words(n, get_list_of_words(text))
         start_time = process_time()
-        for word in first_n_words:
-            find_KMP(word, text)
+        for i in range(n):
+            find_KMP(list_of_words[i], text)
         end_time = process_time()
-        finding_times.append(end_time - start_time)
-        print(end_time - start_time)
+        result = end_time - start_time
+        finding_times.append(result)
+        print(f"KMP: {result}")
     if gc_old:
         gc.enable()
     return finding_times
 
 
-def measure_find_KR_timew(n_values, text):
+def measure_find_KR_times(n_values, list_of_words, text):
     finding_times = []
     gc_old = gc.isenabled()
     gc.disable()
     for n in n_values:
-        first_n_words = get_n_first_words(n, get_list_of_words(text))
         start_time = process_time()
-        for word in first_n_words:
-            find_KR(word, text)
+        for i in range(n):
+            find_KR(list_of_words[i], text)
         end_time = process_time()
-        finding_times.append(end_time - start_time)
+        result = end_time - start_time
+        finding_times.append(result)
+        print(f"KR: {result}")
     if gc_old:
         gc.enable()
     return finding_times
 
 
-def plot_results():
+def main():
     n_values = list(range(100, 1100, 100))
     text = get_text('../Sorts/pan-tadeusz-unix.txt')
+    list_of_words = get_list_of_words(text)
     plt.figure(figsize=(12, 6))
-    plt.plot(n_values, measure_find_N_timew(n_values, text), label="Naive algorithm")
-    plt.plot(n_values, measure_find_KMP_timew(n_values, text), label="KMP algorithm")
-    plt.plot(n_values, measure_find_KR_timew(n_values, text), label="KR algorithm")
+    plt.plot(n_values, measure_find_N_times(n_values, list_of_words, text), label="Naive algorithm")
+    plt.plot(n_values, measure_find_KMP_times(n_values, list_of_words, text), label="KMP algorithm")
+    plt.plot(n_values, measure_find_KR_times(n_values, list_of_words, text), label="KR algorithm")
     plt.xlabel("Liczba słów")
     plt.ylabel("Czas wyszukiwania [s]")
     plt.legend()
     plt.savefig('finding_times.png')  # Save the figure to a file
     plt.clf()  # Clear the figure
 
-if __name__ == '__main__':
-    plot_results()
+
+if __name__ == "__main__":
+    main()
