0d139765597feeb4cd1b3cc780a04f9da899ebbf
mikrozek <01178582@pw.edu.pl>
2023-05-16 14:36:20 +0200

    commit before pulling

---
+++ Pattern matching/test_algorithms.py
@@ -0,0 +1,39 @@
+import random
+import string
+from kr_algorithm import find_KR
+from Naive_algorithm import find_KMP, find_N
+
+
+def test_string_matching_algorithms():
+    ALPHABET = string.ascii_letters[:2]  # Limit alphabet to two letters
+    NUMBER_OF_TESTS = 10
+    MAX_TEXT_LENGTH = 100
+    MAX_PATTERN_LENGTH = 10
+
+    for _ in range(NUMBER_OF_TESTS):
+        text_length = random.randint(1, MAX_TEXT_LENGTH)
+        pattern_length = random.randint(1, min(text_length, MAX_PATTERN_LENGTH))
+
+        text = ''.join(random.choices(ALPHABET, k=text_length))
+        pattern = ''.join(random.choices(ALPHABET, k=pattern_length))
+
+        text = 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+        pattern = 'bbb'
+
+        kr_result = find_KR(pattern, text)
+        kmp_result = find_KMP(pattern, text)
+        naive_result = find_N(pattern, text)
+
+        print(f"Text: {text}")
+        print(f"Pattern: {pattern}")
+
+        assert kr_result == kmp_result == naive_result, \
+            "The algorithms returned different results!"
+
+        print(f"KR algorithm returned: {kr_result}\n")
+        print(f"KMP algorithm returned: {kmp_result}\n")
+        print(f"NAIVE algorithm returned: {naive_result}\n")
+
+
+if __name__ == "__main__":
+    test_string_matching_algorithms()
