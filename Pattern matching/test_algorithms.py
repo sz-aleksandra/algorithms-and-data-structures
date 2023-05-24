5b08b8cfc733e073f6ba274d81d1739a8f7d2224
Aleksandra Szymańska <01169248@pw.edu.pl>
2023-05-24 23:31:17 +0200

    added 3 tests

--- Pattern matching/test_algorithms.py
+++ Pattern matching/test_algorithms.py
@@ -28,6 +28,11 @@ def test_find_N_string_not_in_text():
     assert positions == []
 
 
+def test_find_N_string_two_times():
+    positions = find_N('ciekawy', 'ciekawy wyjazd z ciekawymi przyjaciolmi')
+    assert positions == [0, 17]
+
+
 def test_find_KMP_empty_string():
     positions = find_KMP('', 'Ciekawy wyjazd z przyjaciolmi')
     assert positions == []
@@ -53,6 +58,11 @@ def test_find_KMP_string_not_in_text():
     assert positions == []
 
 
+def test_find_KMP_string_two_times():
+    positions = find_KMP('ciekawy', 'ciekawy wyjazd z ciekawymi przyjaciolmi')
+    assert positions == [0, 17]
+
+
 def test_find_KR_empty_string():
     positions = find_KR('', 'Ciekawy wyjazd z przyjaciolmi')
     assert positions == []
@@ -76,3 +86,8 @@ def test_find_KR_string_longer_than_text():
 def test_find_KR_string_not_in_text():
     positions = find_KR('admaskldmdasda', 'Ciekawy wyjazd z przyjaciolmi')
     assert positions == []
+
+
+def test_find_KR_string_two_times():
+    positions = find_KR('ciekawy', 'ciekawy wyjazd z ciekawymi przyjaciolmi')
+    assert positions == [0, 17]
